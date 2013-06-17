21f3f6953eb70421ccaf5f14faa5f26f29b91a5d
Diego Barreto <diegobns@hotmail.com>
2013-06-17 17:03:42 -0300

    Finalizando
    Finalizando

--- app/models/fexaord.rb
+++ app/models/fexaord.rb
@@ -2,4 +2,5 @@ class Fexaord < ActiveRecord::Base
   belongs_to :ordem
   attr_accessible :ordem_id 
   # attr_accessible :title, :body
+  validates_uniqueness_of   :ordem_id, message:" ja foi fechada, por favor, verifique o ID"
 end
