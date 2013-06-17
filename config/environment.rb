fb70fde7c2cfeb377ac22dc631d398e340c6d499
Alyson <alyson8989@hotmail.com>
2013-06-17 16:58:43 -0300

    Correções
    Correções

--- config/environment.rb
+++
@@ -1,5 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Projeto4::Application.initialize!
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Projeto4::Application.config.secret_token = '1d650b7bbbc11d6c89479be355387620a781925369cd97292df5f3d53d98ca78659334753d1ddfc1c58ccdba355d2d95b929916119ec0242895fe892de37d973'
