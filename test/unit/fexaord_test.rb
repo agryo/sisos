f8cb6ee4f4393e5f1df7cdcbca5e6942675853a7
Augusto <augustofarias_@hotmail.com>
2013-06-17 16:56:57 -0300

    Atualizações
    Atualizações

---
+++ test/unit/fexaord_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FexaordTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/funcionario_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FuncionarioHelperTest < ActionView::TestCase
+end
